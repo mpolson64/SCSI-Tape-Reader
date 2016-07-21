7d79e5116a83da98b06a501e6df77a999dd4a797
Sofia Kwok <zazakwok@gmail.com>
2016-07-21 11:31:48 -0500

    made RawScan and debugged

--- Card.java
+++ Card.java
@@ -1,16 +1,18 @@
+import java.util.ArrayList;
+
 public class Card implements OperationCard, NumberCard {
-    private ArrayList<boolean> filteredScan;
+    private ArrayList<Boolean> filteredScan;
 
     public Card(RawScan scan) {
 
     }
 
-    public ArrayList<boolean> getScan() {
+    public ArrayList<Boolean> getScan() {
         return filteredScan;
     }
 
-    public int binaryRead(ArrayList<boolean> list) {
-        int number;
+    public int binaryRead(ArrayList<Boolean> list) {
+        int number = 0;
         for(int i = 0; i < list.size(); i++) {
             if(list.get(i)) {
                 number += Math.pow(i, 2);
@@ -20,17 +22,17 @@ public class Card implements OperationCard, NumberCard {
     }
 
     public byte getOperation() {    //0: add, 1:subtract, 2: multiply, 3: divide
-        if (card.get(0) == false && card.get(1) == false){
-            operation = 0;
+        if (filteredScan.get(0) == false && filteredScan.get(1) == false){
+            return 0;
         }
-        else if (card.get(0) == false && card.get(1) == true){
-            operation = 1;
+        else if (filteredScan.get(0) == false && filteredScan.get(1) == true){
+            return 1;
         }
-        else if (card.get(0) == true && card.get(1) == false){
-            operation = 2;
+        else if (filteredScan.get(0) == true && filteredScan.get(1) == false){
+            return 2;
         }
         else {
-            operation = 3;
+            return 3;
         }
     }
 
@@ -39,17 +41,43 @@ public class Card implements OperationCard, NumberCard {
     }
 
     public byte[] getNotes() {
-        ArrayList<byte> temp0 = new ArrayList<byte>();
+        ArrayList<Byte> temp0 = new ArrayList<Byte>();
 
         for(int i = 0; i < filteredScan.size(); i += 2) {
-            ArrayList<byte> temp1 = new ArrayList<byte>();
+            ArrayList<Boolean> temp1 = new ArrayList<Boolean>();
             for(int j = 0; j < 4; j++) {
                 temp1.add(filteredScan.get(i));
             }
-            temp0.add(binaryRead(temp1));
+            temp0.add((byte)binaryRead(temp1));
         }
 
-        return temp0.toArray();
+        byte[] out = new byte[filteredScan.size() / 2];
+		
+		for(int i = 0; i < out.length; i++) {
+			out[i] = temp0.get(i);
+		}
+		
+        return out;
+    }
+	
+	public byte[] getTimings() {
+        ArrayList<Byte> temp0 = new ArrayList<Byte>();
+
+        for(int i = 1; i < filteredScan.size(); i += 2) {
+            ArrayList<Boolean> temp1 = new ArrayList<Boolean>();
+            for(int j = 0; j < 4; j++) {
+                temp1.add(filteredScan.get(i));
+            }
+            temp0.add((byte)binaryRead(temp1));
+        }
+
+		byte[] out = new byte[filteredScan.size() / 2];
+		
+		for(int i = 0; i < out.length; i++) {
+			out[i] = temp0.get(i);
+		}
+		
+        return out;
     }
 
 
---
+++ RawScan.java
@@ -0,0 +1,5 @@
+public class RawScan {
+	public RawScan() {
+		
+	}
+}
